ccecdbc8d20b1dd07dad499eb3af12b15187e26d
Jandullaev <jandullaevallayar@gmail.com>
2024-12-06 21:47:50 +0500

    added sc2

--- src/main/java/com/epam/project/service/UserService.java
+++ src/main/java/com/epam/project/service/UserService.java
@@ -4,7 +4,7 @@ import com.epam.project.model.User;
 
 public class UserService {
     private static final String USER_EMAIL = "user.email";
-    private static final String USER_PASSWORD = "user.password";
+    private static final String USER_PASSWORD = "USER_PASSWORD";
 
     public static User withCredentialsFromProperty() {
         return new User(TestDataReader.getTestData(USER_EMAIL), System.getenv(USER_PASSWORD));
